37e1574b5704b5d54fdecb5b0941308a21d4a4b4
Renan Conessa <renanconessa@zup.com.br>
2020-07-22 15:00:04 -0300

    feat: add login view

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,15 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: 'devolopment',
+  mode: 'development',
   entry: './src/main/index.tsx',
   output: {
     path: path.join(__dirname, 'public/js'),
     publicPath: './public/js',
-    fileName: 'bundle.js'
+    filename: 'bundle.js'
   },
   resolve: {
-    extesions: ['.ts', '.tsx', '.js', '.scss'],
+    extensions: ['.ts', '.tsx', '.js', '.scss'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
@@ -19,7 +19,7 @@ module.exports = {
     rules: [{
       test: /\.ts(x?)$/,
       loader: 'ts-loader',
-      explude: /node_modules/
+      exclude: /node_modules/
     }, {
       test: /\.scss$/,
       use: [{
