4007e0e2b66672f701d373f0be1b9704a5ff6378
Renan Conessa <renanconessa@zup.com.br>
2020-07-22 13:45:55 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,29 @@ module.exports = {
     fileName: 'bundle.js'
   },
   resolve: {
-    extesions: ['.ts', '.tsx', '.js'],
+    extesions: ['.ts', '.tsx', '.js', '.scss'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
+  },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      explude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        options: {
+          modules: true
+        }
+      }, {
+        loader: 'sass-loader'
+      }]
+    }]
   },
   devServer: {
     contentBase: './public',
