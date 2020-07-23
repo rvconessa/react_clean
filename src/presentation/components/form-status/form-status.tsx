a10a49ef60c8afd198271d5651f35cb78643b36d
Renan Conessa <renanconessa@zup.com.br>
2020-07-23 17:46:11 -0300

    feat: ensure login present spinner on submit

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -4,7 +4,8 @@ import Spinner from '@/presentation/components/spinner/spinner'
 import Context from '@/presentation/context/form/form-context'
 
 const FormStatus: React.FC = () => {
-  const { isLoading, mainError } = useContext(Context)
+  const { state: { isLoading, mainError } } = useContext(Context)
+
   return (
     <div data-testid="error-wrap" className={Styles.errorWrap}>
       { isLoading && <Spinner className={Styles.spinner} /> }
