3432f1ad2373353b83c01c88c5b942f9d063c1a6
Renan Conessa <renanconessa@zup.com.br>
2020-07-24 16:53:53 -0300

    feat: ensure ValidationBuilder has a min length helper

--- src/validation/validators/build/validation-builder.ts
+++ src/validation/validators/build/validation-builder.ts
@@ -1,3 +1,4 @@
+import { MinLengthValidation } from './../min-length/min-length-validator'
 import { EmailValidation } from './../email/email-validation'
 import { FieldValidation } from '@/validation/protocols/field-validation'
 import { RequiredFieldValidation } from '@/validation/validators'
@@ -19,6 +20,11 @@ export class ValidationBuilder {
 
   email (): ValidationBuilder {
     this.validations.push(new EmailValidation(this.fieldName))
+    return this
+  }
+
+  min (length: number): ValidationBuilder {
+    this.validations.push(new MinLengthValidation(this.fieldName, length))
     return this
   }
 
