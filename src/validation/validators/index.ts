dd0c1d65af755f306bd6840aefdb024748ed2369
Renan Conessa <renanconessa@zup.com.br>
2020-07-24 16:49:08 -0300

    feat: ensure ValidationBuilder has a required helper

---
+++ src/validation/validators/index.ts
@@ -0,0 +1,4 @@
+export * from './email/email-validation'
+export * from './min-length/min-length-validator'
+export * from './required-field/required-fiel-validation'
+export * from './validation-composite/validation-composite'
